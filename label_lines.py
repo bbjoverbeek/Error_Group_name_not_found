e439ce80f6a0507b207981957c6765c1755c8757
Bjurn Overbeek <bbjoverbeek@gmail.com>
2021-03-05 21:34:02 +0100

    M working now too. Module is fully functional for the Shrek script

--- label_lines.py
+++ label_lines.py
@@ -11,16 +11,26 @@ def main(argv):
 
     filename = argv[1]
 
+    metadata = True
+    newline_count = 0
+
     with open(filename, 'r') as inp:
         for line in inp.readlines():
-            if re.match(r'(\s)\1{23}', line):
-                line = re.sub(r'(\s)\1{23}', 'C|\t\t\t\t\t', line)
-            elif re.match(r'(\s)\1{11}', line):
-                line = re.sub(r'(\s)\1{11}', 'D|\t\t\t', line)
-            elif line.isupper():
-                line = re.sub(r'(.+)', r'S|\t\1', line)
-            elif line != "\n":
-                line = "N|\t" + line
+            if metadata == True:
+                line = re.sub(r'(.+)', r'M|\1', line)
+                if line == '\n':
+                    newline_count += 1 
+                    if newline_count == 5:
+                        metadata = False
+            if metadata == False:
+                if re.match(r'(\s)\1{23}', line):
+                    line = re.sub(r'(\s)\1{23}', 'C|\t\t\t\t\t\t', line)
+                elif re.match(r'(\s)\1{11}', line):
+                    line = re.sub(r'(\s)\1{11}', 'D|\t\t\t', line)
+                elif line.isupper():
+                    line = re.sub(r'(.+)', r'S|\t\1', line)
+                elif line != "\n":
+                    line = "N|\t" + line
 
             print(line)
 
